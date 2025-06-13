d240daf1b37339cef3c260c23d5dbdb70303a9a8
Irshad Shaikh <129972530+irshad317-gif@users.noreply.github.com>
2025-06-13 21:52:19 +0530

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,6 @@ export default function Index() {
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
         backgroundColor: "#f0f0f0",
       }}
     >
